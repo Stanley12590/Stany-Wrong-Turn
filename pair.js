9f6ee8474d003590b85938092e239f608dac3b14
Stanley12590 <masanyiwastanley378@gmail.com>
2025-11-05 02:19:00 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -13,19 +13,15 @@ const MESSAGE = process.env.MESSAGE || `
 𝐅𝐎𝐋𝐋𝐎𝐖 𝐎𝐔𝐑 𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏 𝐂𝐇𝐀𝐍𝐍𝐄𝐋
 
 > *STANY PROGRAMING HUB*
-
 > https://whatsapp.com/channel/0029VbBy4ON0wajpzLTzom35
 
 > *STANY PROGRAMING HUB2*
-
 > https://whatsapp.com/channel/0029Vb72cVkJ3jv10gzqTn18
 
 > *STANY BETTING AND CASINOS*
-
 > https://whatsapp.com/channel/0029VbBnoamIHphCnwau5Z3E
 
 > 𝐅𝐎𝐑𝐊 𝐀𝐍𝐃 𝐒𝐓𝐀𝐑 𝐓𝐇𝐄 𝐑𝐄𝐏𝐎
-
 > github.com/Stanley12590/STANY_WRONG_TURN_6
 
 > StanyTz🏻
@@ -67,7 +63,7 @@ router.get('/', async (req, res) => {
                 num = num.replace(/[^0-9]/g, '');
                 const code = await Smd.requestPairingCode(num);
                 if (!res.headersSent) {
-                    await res.send({ code });
+                    res.send({ code });
                 }
             }
 
@@ -78,57 +74,60 @@ router.get('/', async (req, res) => {
                 if (connection === "open") {
                     try {
                         await delay(10000);
-                        if (fs.existsSync('./auth_info_baileys/creds.json'));
+                        if (fs.existsSync('./auth_info_baileys/creds.json')) {
+                            const auth_path = './auth_info_baileys/';
+                            let user = Smd.user.id;
 
-                        const auth_path = './auth_info_baileys/';
-                        let user = Smd.user.id;
-
-                        function randomMegaId(length = 6, numberLength = 4) {
-                            const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-                            let result = '';
-                            for (let i = 0; i < length; i++) {
-                                result += characters.charAt(Math.floor(Math.random() * characters.length));
-                            }
-                            const number = Math.floor(Math.random() * Math.pow(10, numberLength));
-                            return `${result}${number}`;
-                        }
-
-                        const mega_url = await upload(fs.createReadStream(auth_path + 'creds.json'), `${randomMegaId()}.json`);
-                        const Id_session = mega_url.replace('https://mega.nz/file/', '');
-                        const Scan_Id = Id_session;
-
-                        // Send session ID
-                        let msgsss = await Smd.sendMessage(user, { text: Scan_Id });
-
-                        // Send banner image with caption as a forwarded message from the newsletter channel
-                        await Smd.sendMessage(user, {
-                            image: { url: "https://files.catbox.moe/23j7tl.jpg" },
-                            caption: MESSAGE,
-                            contextInfo: {
-                                forwardingScore: 999,
-                                isForwarded: true,
-                                externalAdReply: {
-                                    showAdAttribution: true,
-                                    title: "STANY WRONG TURNS",
-                                    body: "WhatsApp Channel",
-                                    previewType: "PHOTO",
-                                    thumbnailUrl: "https://files.catbox.moe/v1lazm.png",
-                                    mediaType: 1,
-                                    mediaUrl: "https://files.catbox.moe/23j7tl.jpg",
-                                    sourceUrl: "https://whatsapp.com/channel/0029VbBy4ON0wajpzLTzom35"
+                            function randomMegaId(length = 6, numberLength = 4) {
+                                const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+                                let result = '';
+                                for (let i = 0; i < length; i++) {
+                                    result += characters.charAt(Math.floor(Math.random() * characters.length));
                                 }
+                                const number = Math.floor(Math.random() * Math.pow(10, numberLength));
+                                return `${result}${number}`;
                             }
-                        }, { quoted: msgsss });
 
-                        await delay(1000);
-                        try { await fs.emptyDirSync(__dirname + '/auth_info_baileys'); } catch (e) { }
+                            const mega_url = await upload(fs.createReadStream(auth_path + 'creds.json'), `${randomMegaId()}.json`);
+                            const Id_session = mega_url.replace('https://mega.nz/file/', '');
+                            const Scan_Id = Id_session;
 
+                            // Send session ID
+                            let msgsss = await Smd.sendMessage(user, { text: Scan_Id });
+
+                            // Send banner image with caption
+                            await Smd.sendMessage(user, {
+                                image: { url: "https://files.catbox.moe/23j7tl.jpg" },
+                                caption: MESSAGE,
+                                contextInfo: {
+                                    forwardingScore: 999,
+                                    isForwarded: true,
+                                    externalAdReply: {
+                                        showAdAttribution: true,
+                                        title: "STANY WRONG TURNS",
+                                        body: "WhatsApp Channel",
+                                        previewType: "PHOTO",
+                                        thumbnailUrl: "https://files.catbox.moe/v1lazm.png",
+                                        mediaType: 1,
+                                        mediaUrl: "https://files.catbox.moe/23j7tl.jpg",
+                                        sourceUrl: "https://whatsapp.com/channel/0029VbBy4ON0wajpzLTzom35"
+                                    }
+                                }
+                            }, { quoted: msgsss });
+
+                            await delay(1000);
+                            try { 
+                                await fs.emptyDirSync(__dirname + '/auth_info_baileys'); 
+                            } catch (e) { }
+                        }
                     } catch (e) {
                         console.log("Error during file upload or message send: ", e);
                     }
 
                     await delay(100);
-                    await fs.emptyDirSync(__dirname + '/auth_info_baileys');
+                    try {
+                        await fs.emptyDirSync(__dirname + '/auth_info_baileys');
+                    } catch (e) { }
                 }
 
                 if (connection === "close") {
@@ -155,14 +154,17 @@ router.get('/', async (req, res) => {
             console.log("Error in SUHAIL function: ", err);
             exec('pm2 restart qasim');
             console.log("Service restarted due to error");
-            SUHAIL();
-            await fs.emptyDirSync(__dirname + '/auth_info_baileys');
+            await SUHAIL();
+            try {
+                await fs.emptyDirSync(__dirname + '/auth_info_baileys');
+            } catch (e) { }
             if (!res.headersSent) {
-                await res.send({ code: "Try After Few Minutes" });
+                res.send({ code: "Try After Few Minutes" });
             }
         }
     }
 
+    // FIXED: Remove the syntax error - call the function properly
     await SUHAIL();
 });
 
